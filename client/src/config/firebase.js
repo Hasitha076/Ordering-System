6ee1e7f99ba5be825ce7c45aa7e6cefcda4cae7b
Hasitha Madushan Epa <88420265+Hasitha076@users.noreply.github.com>
2024-05-09 00:42:55 +0530

    Update firebase.js

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,1 +1,19 @@
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIsVhZnYeWxRHsdha_fek0KunXNFP1Tec",
+    authDomain: "order-management-system-766af.firebaseapp.com",
+    projectId: "order-management-system-766af",
+    storageBucket: "order-management-system-766af.appspot.com",
+    messagingSenderId: "958793517870",
+    appId: "1:958793517870:web:0bb1ad5a5cccaa9fbb88bf",
+    measurementId: "G-KNRW7BD783"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { app, storage };
