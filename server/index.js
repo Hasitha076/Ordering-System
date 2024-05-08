eb606a40549f5f514c7f9e81f2d0c3175be0ab48
Hasitha Madushan Epa <88420265+Hasitha076@users.noreply.github.com>
2024-05-09 01:47:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const connectDB = require('./config/db')
 connectDB()
 
 // configuration
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 const app = express()
 app.use(express.json())
 app.use(cors({
