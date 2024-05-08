9b96a0f7e744b299271e5efd6df31add6536fe3e
Hasitha Madushan Epa <88420265+Hasitha076@users.noreply.github.com>
2024-05-09 02:15:53 +0530

    Update index.js filess

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'https://ordering-system-1.onrender.com',
+    origin: process.env.URL,
     credentials: true
 }));
 app.use(cookieParser());
